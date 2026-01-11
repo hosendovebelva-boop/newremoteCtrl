9ae2609e816dfa08c50d04bb37852c13575c7634
cyiwen <cyi059067@gmail.com>
2026-01-10 23:22:43 -0600

    完成鼠标消息的处理

--- RemoteCtrl/RemoteCtrl/ServerSocket.h
+++ RemoteCtrl/RemoteCtrl/ServerSocket.h
@@ -135,6 +135,19 @@ public:
 
 #pragma pack(pop)
 
+typedef struct MouseEvent{
+	MouseEvent()
+	{
+		nAction = 0;
+		nButton = -1;
+		ptXY.x = 0;
+		ptXY.y = 0;
+	}
+	WORD nAction;	// µã»÷¡¢ÒÆ¶¯¡¢Ë«»÷
+	WORD nButton;	// ×ó¼ü¡¢ÓÒ¼ü¡¢ÖÐ¼ü
+	POINT ptXY;		// ×ø±ê
+}MOUSEEV, * PMOUSEEV;
+
 class CServerSocket
 {
 public:
@@ -226,6 +239,16 @@ public:
 		if ((m_packet.sCmd >= 2) && (m_packet.sCmd <= 4))
 		{
 			strPath = m_packet.strData;
+			return true;
+		}
+		return false;
+	}
+
+	bool GetMouseEvent(MOUSEEV& mouse)
+	{
+		if (m_packet.sCmd == 5)
+		{
+			memcpy(&mouse, m_packet.strData.c_str(), sizeof(MOUSEEV));
 			return true;
 		}
 		return false;
