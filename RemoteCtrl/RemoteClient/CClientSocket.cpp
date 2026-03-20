56ff64e2474c16832521d48e3f116fa832bd3336
cyiwen <cyi059067@gmail.com>
2026-03-20 04:31:33 -0500

    1 尝试解决多线程发送导致的冲突 备注： 发现新的问题，对于文件/文件夹信息获取

--- RemoteCtrl/RemoteClient/CClientSocket.cpp
+++ RemoteCtrl/RemoteClient/CClientSocket.cpp
@@ -37,3 +37,55 @@ void Dump(BYTE* pData, size_t nSize)
 	strOut += '\n';
 	OutputDebugStringA(strOut.c_str());
 }
+
+void CClientSocket::threadEntry(void* arg)
+{
+	CClientSocket* thiz = (CClientSocket*)arg;
+	thiz->threadFunc();
+}
+
+void CClientSocket::threadFunc()
+{
+	if (InitSocket() == false)
+	{
+		return;
+	}
+	std::string strBuffer;
+	strBuffer.resize(BUFFER_SIZE);
+	char* pBuffer = (char*)strBuffer.c_str();
+	int index = 0;
+	while (m_sock != INVALID_SOCKET)
+	{
+		if (m_lstSend.size() > 0)
+		{
+			CPacket& head = m_lstSend.front();
+			if (Send(head) == false)
+			{
+				TRACE("发送失败!\r\n");
+				continue;
+			}
+			auto pr = m_mapAck.insert(std::pair<HANDLE, 
+				std::list<CPacket>>(head.hEvent, std::list<CPacket>()));
+			std::list<CPacket> lstRecv;
+			int length = recv(m_sock, pBuffer + index, BUFFER_SIZE - index, 0);
+			if (length > 0 || index > 0)
+			{
+				index += length;
+				size_t size = (size_t)index;
+				CPacket pack((BYTE*)pBuffer, size);
+				if (size > 0)
+				{
+					// TODO:通知对应的事件
+					pack.hEvent = head.hEvent;
+					pr.first->second.push_back(pack);
+					SetEvent(head.hEvent);
+				}
+			}
+			else if (length <= 0 && index <= 0)
+			{
+				CloseSocket();
+			}
+			m_lstSend.pop_front();
+		}
+	}
+}
