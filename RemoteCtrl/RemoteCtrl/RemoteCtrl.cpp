c49dfbcc3fd94f91d30bb6a7e628ca0ef5950e41
cyiwen <cyi059067@gmail.com>
2026-03-31 22:18:38 +0800

    1.追加启动权限请求 2.完成了开机启动的功能

--- RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
+++ RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
@@ -13,47 +13,102 @@
 // 唯一的应用程序对象
 CWinApp theApp;
 using namespace std;
+//开机启动的时候，程序的权限是跟随启动用户的
+//如果两者的权限不一致，则会爆汁程序启动失败
+//开机启动对环境变量有影响，如果依赖dll（动态库），则可能会启动失败
+//复制这些dll到system32下面
+void ChooseAutoInvoke()
+{
+	TCHAR wcsSystem[MAX_PATH] = _T("");
+	CString strPath = CString(_T("C:\\Windows\\system32\\RemoteCtrl.exe"));
+	if (PathFileExists(strPath))
+	{
+		return;
+	}
+	CString strSubKey = _T("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run");
+	CString strInfo = _T("该程序只允许用于合法的用途!\n");
+	strInfo += _T("继续运行该程序将使得这台机器处于被监控状态！\n");
+	strInfo += _T("如果你不希望这样请点击“取消”按钮，退出程序！\n");
+	strInfo += _T("按下“是”按钮，该程序将被复制到你的机器上，并随系统启动而自动运行！\n");
+	strInfo += _T("按下“否”按钮，该程序只允许一次，不会在系统内留下任何东西！\n");
+	strInfo += _T("\n");
+	int ret = MessageBox(NULL, strInfo, _T("警告"), MB_YESNOCANCEL | MB_ICONWARNING | MB_TOPMOST);
+	if (ret == IDYES)
+	{
+		char sPath[MAX_PATH] = "";
+		char sSys[MAX_PATH] = "";
+		std::string strExe = "\\RemoteCtrl.exe ";
+		GetCurrentDirectoryA(MAX_PATH, sPath);
+		GetSystemDirectoryA(sSys, sizeof(sSys));
+		std::string strCmd = "mklink " + std::string(sSys) + strExe + std::string(sPath) + strExe;
+		ret = system(strCmd.c_str());
+		TRACE("strCmd = %s ret = %d\r\n", strCmd, ret);
+		HKEY hKey = NULL;
+		ret = RegOpenKeyEx(HKEY_LOCAL_MACHINE, strSubKey, 0, KEY_ALL_ACCESS | KEY_WOW64_64KEY, &hKey);
+		if (ret != ERROR_SUCCESS)
+		{
+			RegCloseKey(hKey);
+			MessageBox(NULL, _T("设置自动开机失败！是否因为权限不足？"), _T("错误"), MB_ICONERROR | MB_TOPMOST);
+			::exit(0);
+		}
+
+		ret = RegSetValueEx(hKey, _T("RemoteCtrl"), 0, REG_EXPAND_SZ, (BYTE*)(LPCTSTR)strPath, strPath.GetLength() * sizeof(TCHAR));
+		if (ret != ERROR_SUCCESS)
+		{
+			RegCloseKey(hKey);
+			MessageBox(NULL, _T("设置自动开机失败！是否因为权限不足？"), _T("错误"), MB_ICONERROR | MB_TOPMOST);
+			::exit(0);
+		}
+		RegCloseKey(hKey);
+
+	}
+	else if (ret == IDCANCEL)
+	{
+
+	}
+} 
 
 int main()
 {
-    int nRetCode = 0;
+	int nRetCode = 0;
 
-    HMODULE hModule = ::GetModuleHandle(nullptr);
+	HMODULE hModule = ::GetModuleHandle(nullptr);
 
-    if (hModule != nullptr)
-    {
-        // 初始化 MFC 并在失败时显示错误
-        if (!AfxWinInit(hModule, nullptr, ::GetCommandLine(), 0))
-        {
-            // TODO: 在此处为应用程序的行为编写代码。
-            wprintf(L"错误: MFC 初始化失败\n");
-            nRetCode = 1;
-        }
-        else
-        {
-            CCommand cmd;
-            int ret = CServerSocket::getInstance()->Run(&CCommand::RunCommand, &cmd);
-            switch (ret)
-            {
-            case -1:
-                MessageBox(NULL, _T("网络初始化异常，未能成功初始化，请检查网络状态！"), _T("网络初始化失败"), MB_OK | MB_ICONERROR);
-                exit(0);
-                break;
-            case -2:
-                MessageBox(NULL, _T("无法正常接入用户，自动重试"), _T("接入用户失败"), MB_OK | MB_ICONERROR);
-                exit(0);
-                break;
-            default:
-                break;
-            }
-        }
-    }
-    else
-    {
-        // TODO: 更改错误代码以符合需要
-        wprintf(L"错误: GetModuleHandle 失败\n");
-        nRetCode = 1;
-    }
+	if (hModule != nullptr)
+	{
+		// 初始化 MFC 并在失败时显示错误
+		if (!AfxWinInit(hModule, nullptr, ::GetCommandLine(), 0))
+		{
+			// TODO: 在此处为应用程序的行为编写代码。
+			wprintf(L"错误: MFC 初始化失败\n");
+			nRetCode = 1;
+		}
+		else
+		{
+			CCommand cmd;
+			ChooseAutoInvoke();
+			int ret = CServerSocket::getInstance()->Run(&CCommand::RunCommand, &cmd);
+			switch (ret)
+			{
+			case -1:
+				MessageBox(NULL, _T("网络初始化异常，未能成功初始化，请检查网络状态！"), _T("网络初始化失败"), MB_OK | MB_ICONERROR);
+				::exit(0);
+				break;
+			case -2:
+				MessageBox(NULL, _T("无法正常接入用户，自动重试"), _T("接入用户失败"), MB_OK | MB_ICONERROR);
+				::exit(0);
+				break;
+			default:
+				break;
+			}
+		}
+	}
+	else
+	{
+		// TODO: 更改错误代码以符合需要
+		wprintf(L"错误: GetModuleHandle 失败\n");
+		nRetCode = 1;
+	}
 
-    return nRetCode;
+	return nRetCode;
 }
