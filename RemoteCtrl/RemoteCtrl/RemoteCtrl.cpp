7f1277456ad4d39be98e12b513dd6e72cb9057ac
cyiwen <cyi059067@gmail.com>
2026-04-03 20:52:16 +0800

    1 代码重构 2 代码优化

--- RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
+++ RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
@@ -10,6 +10,9 @@
 #define new DEBUG_NEW
 #endif
 
+//#define INVOKE_PATH_T _T("C:\\Windows\\system32\\RemoteCtrl.exe")
+#define INVOKE_PATH_T _T("C:\\Users\\49522\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\RemoteCtrl.exe")
+
 // 唯一的应用程序对象
 CWinApp theApp;
 using namespace std;
@@ -17,59 +20,10 @@ using namespace std;
 //如果两者的权限不一致，则会爆汁程序启动失败
 //开机启动对环境变量有影响，如果依赖dll（动态库），则可能会启动失败
 //复制这些dll到system32下面
-
-void WriteRegisterTable(const CString& strPath)
-{
-	CString strSubKey = _T("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run");
-
-	char sPath[MAX_PATH] = "";
-	char sSys[MAX_PATH] = "";
-	std::string strExe = "\\RemoteCtrl.exe ";
-	GetCurrentDirectoryA(MAX_PATH, sPath);
-
-	GetSystemDirectoryA(sSys, sizeof(sSys));
-	std::string strCmd = "mklink " + std::string(sSys) + strExe + std::string(sPath) + strExe;
-	int ret = system(strCmd.c_str());
-	TRACE("strCmd = %s ret = %d\r\n", strCmd, ret);
-	HKEY hKey = NULL;
-	ret = RegOpenKeyEx(HKEY_LOCAL_MACHINE, strSubKey, 0, KEY_ALL_ACCESS | KEY_WOW64_64KEY, &hKey);
-	if (ret != ERROR_SUCCESS)
-	{
-		RegCloseKey(hKey);
-		MessageBox(NULL, _T("设置自动开机失败！是否因为权限不足？"), _T("错误"), MB_ICONERROR | MB_TOPMOST);
-		::exit(0);
-	}
-
-	ret = RegSetValueEx(hKey, _T("RemoteCtrl"), 0, REG_EXPAND_SZ, (BYTE*)(LPCTSTR)strPath, strPath.GetLength() * sizeof(TCHAR));
-	if (ret != ERROR_SUCCESS)
-	{
-		RegCloseKey(hKey);
-		MessageBox(NULL, _T("设置自动开机失败！是否因为权限不足？"), _T("错误"), MB_ICONERROR | MB_TOPMOST);
-		::exit(0);
-	}
-	RegCloseKey(hKey);
-}
-
-/*
-
-*/
-void WriteStartupDir(const CString& strPath)
-{
-	CString strCmd = GetCommandLine();
-	strCmd.Replace(_T("\""), _T(""));
-	BOOL ret = CopyFile(strCmd, strPath, FALSE);
-	if (ret == FALSE)
-	{
-		MessageBox(NULL, _T("复制文件失败，是否权限不足？\r\n"), _T("错误"), MB_ICONERROR | MB_TOPMOST);
-		::exit(0);
-	}
-}
-
-void ChooseAutoInvoke()
+bool ChooseAutoInvoke(const CString& strPath)
 {
 	TCHAR wcsSystem[MAX_PATH] = _T("");
-	//CString strPath = CString(_T("C:\\Windows\\system32\\RemoteCtrl.exe"));
-	CString strPath = _T("C:\\Users\\49522\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\RemoteCtrl.exe");
+	//CString strPath = CString();
 	if (PathFileExists(strPath))
 	{
 		return;
@@ -84,141 +38,46 @@ void ChooseAutoInvoke()
 	if (ret == IDYES)
 	{
 		//WriteRegisterTable(strPath);
-		WriteStartupDir(strPath);
+		if (!CEdoyunTool::WriteStartupDir(strPath))
+		{
+			MessageBox(NULL, _T("复制文件失败，是否权限不足？\r\n"), _T("错误"), MB_ICONERROR | MB_TOPMOST);
+			return false;
+		}
 	}
 	else if (ret == IDCANCEL)
 	{
-
-	}
-}
-
-void ShowError()
-{
-	LPWSTR lpMessageBuf = NULL;
-	//strerror(errno); 标准
-	FormatMessage(
-		FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER,
-		NULL, GetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPWSTR)&lpMessageBuf, 0, NULL);
-	OutputDebugString(lpMessageBuf);
-	MessageBox(NULL, lpMessageBuf, _T("发生错误"), 0);
-
-	LocalFree(lpMessageBuf);
-}
-
-bool IsAdmin()
-{
-	HANDLE hToken = NULL;
-	if (!OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &hToken))
-	{
-		ShowError();
 		return false;
 	}
-	TOKEN_ELEVATION eve;
-	DWORD len = 0;
-	if (GetTokenInformation(hToken, TokenElevation, &eve, sizeof(eve), &len) == FALSE)
-	{
-		ShowError();
-		return false;
-	}
-	CloseHandle(hToken);
-
-	if (len == sizeof(eve))
-	{
-		return eve.TokenIsElevated;
-	}
-	printf("length of tokeninformation is %d\r\n", len);
-	return false;
-}
-
-void RunAsAdmin()
-{
-	//TODO：获取管理员权限、使用该权限创建进程
-	HANDLE hToken = NULL;
-	//LOGON32_LOGON_BATCH LOGON32_LOGON_INTERACTIVE
-	BOOL ret = LogonUser(L"Administrator", NULL, NULL, LOGON32_LOGON_BATCH, LOGON32_PROVIDER_DEFAULT, &hToken);
-	if (!ret)
-	{
-		ShowError();
-		MessageBox(NULL, _T("登录错误！"), _T("程序错误"), 0);
-		::exit(0);
-	}
-	OutputDebugString(L"Logon administrator success!\r\n");
-	STARTUPINFO si = { 0 };
-	PROCESS_INFORMATION pi = { 0 };
-	TCHAR sPath[MAX_PATH] = _T("");
-	GetCurrentDirectory(MAX_PATH, sPath);
-	CString strCmd = sPath;
-	strCmd += _T("\\RemoteCtrl.exe");
-
-	//ret = CreateProcessWithTokenW(hToken, LOGON_WITH_PROFILE, NULL, (LPWSTR)(LPCWSTR)strCmd, CREATE_UNICODE_ENVIRONMENT, NULL, NULL, &si, &pi);
-	ret = CreateProcessWithLogonW(_T("Administrator"), NULL, NULL, LOGON_WITH_PROFILE, NULL, (LPWSTR)(LPCWSTR)strCmd, CREATE_UNICODE_ENVIRONMENT, NULL, NULL, &si, &pi);
-	CloseHandle(hToken);
-
-	if (!ret)
-	{
-		ShowError();
-		MessageBox(NULL, strCmd, _T("程序错误"), 0);
-		::exit(0);
-	}
-	WaitForSingleObject(pi.hProcess, INFINITE);
-	CloseHandle(pi.hProcess);
-	CloseHandle(pi.hThread);
-
+	return true;
 }
 
 int main()
 {
-	int nRetCode = 0;
-	HMODULE hModule = ::GetModuleHandle(nullptr);
-
-	if (hModule != nullptr)
+	if (CEdoyunTool::IsAdmin())
 	{
-		// 初始化 MFC 并在失败时显示错误
-		if (!AfxWinInit(hModule, nullptr, ::GetCommandLine(), 0))
+		if (!CEdoyunTool::Init()) return 1;
+		if (ChooseAutoInvoke(INVOKE_PATH_T))
 		{
-			// TODO: 在此处为应用程序的行为编写代码。
-			wprintf(L"错误: MFC 初始化失败\n");
-			nRetCode = 1;
-		}
-		else
-		{
-			if (IsAdmin())
-			{
-				OutputDebugString(L"current is run as administrator!\r\n");
-				//MessageBox(NULL, _T("管理员"), _T("用户状态"), 0);
-			}
-			else
-			{
-				OutputDebugString(L"current is run as normal user!\r\n");
-				RunAsAdmin();
-				//MessageBox(NULL, _T("普通用户"), _T("用户状态"), 0);
-				return nRetCode;
-			}
-
 			CCommand cmd;
-			ChooseAutoInvoke();
 			int ret = CServerSocket::getInstance()->Run(&CCommand::RunCommand, &cmd);
 			switch (ret)
 			{
 			case -1:
 				MessageBox(NULL, _T("网络初始化异常，未能成功初始化，请检查网络状态！"), _T("网络初始化失败"), MB_OK | MB_ICONERROR);
-				::exit(0);
 				break;
 			case -2:
 				MessageBox(NULL, _T("无法正常接入用户，自动重试"), _T("接入用户失败"), MB_OK | MB_ICONERROR);
-				::exit(0);
-				break;
-			default:
 				break;
 			}
 		}
 	}
 	else
 	{
-		// TODO: 更改错误代码以符合需要
-		wprintf(L"错误: GetModuleHandle 失败\n");
-		nRetCode = 1;
+		if (CEdoyunTool::RunAsAdmin() == false)
+		{
+			CEdoyunTool::ShowError();
+			return 1;
+		}
 	}
-
-	return nRetCode;
+	return 0;
 }
